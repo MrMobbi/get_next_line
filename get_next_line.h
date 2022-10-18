f504987b35bd80736290ed2a631ba1d832f4d555
Mathias Julliat <mjulliat@c2r9s3.42lausanne.ch>
2022-10-18 11:56:59 +0200

    creation of the project

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjulliat <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 11:50:35 by mjulliat          #+#    #+#             */
+/*   Updated: 2022/10/18 11:53:47 by mjulliat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef
+# define
+
+#include <unsitd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
